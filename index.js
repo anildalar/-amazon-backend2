e8735098fefe18ce9b6665cfa5347dfd1cd92a73
anildalar <dollorinfotech@gmail.com>
2021-09-18 09:00:56 +0530

    new

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('ok');
+const express = require('express');
+
+const app = express();
+
+
+let port = 3000;
+app.listen(port,()=>{
+    console.log(`The the server is running on port ${ port } `);
+});
